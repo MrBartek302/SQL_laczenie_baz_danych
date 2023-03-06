655d9df2adc0e998d79788bed86e5a38e7cebd16
MrBartek302 <126457666+MrBartek302@users.noreply.github.com>
2023-03-06 10:09:14 +0100

    mój SQL

--- serwer/index.js
+++ serwer/index.js
@@ -7,9 +7,33 @@ const app = express()
 app.use(cors())
 
 const port = 3000
+//łączymy się z bazą danych
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "1pro"
+})
+//potwierdzenie połączenia
+con.connect(function(err){
+    if(err){ 
+        console.log(err)}
+    else console.log("Połączono z bazą danych")    
+})
 
 app.get("/", (req, res)=>{
     res.send("ok")
+})
+
+app.get("/select", (req,res)=>{
+    //pobiera nam z naszej bazy danych to co stworzyliśmy
+    const sql = "SELECT * FROM nwm"
+    //zapytanie
+    con.query(sql, function(err, result, fields){
+        if(err) console.log(err)
+        
+        else res.send(result)
+    })
 })
 
 app.listen(port, ()=>{
